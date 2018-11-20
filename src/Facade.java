5a480e22c573a6269d43b5897fc93254bc83a7d8
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2018-11-20 20:36:30 -0300

    alteracao na facade

--- src/Facade.java
+++ src/Facade.java
@@ -1,2 +1,26 @@
 public class Facade {
+    public void adicionaDoador(String id, String nome, String email, String celular, String classe){
+
+    }
+
+    public String pesquisaUsuarioPorId(String id){
+        return "";
+    }
+
+    public String pesquisaUsuarioPorNome(String nome){
+        return "";
+    }
+
+    public void removeUsuario(String id){
+
+    }
+
+    public void adicionaDescritor(String descricao){
+
+    }
+
+    public void adicionaItemParaDoacao(String idDoador, String descricaoItem, int quantidade, String tags){
+
+    }
+    
 }
